0c35654a5f86f02330d57198bbc51576587706eb
Athe-kunal <athekunal@gmail.com>
2026-05-09 20:18:22 -0500

    infra code

--- nanoopd/fsdp/base.py
+++ nanoopd/fsdp/base.py
@@ -35,6 +35,14 @@ class FSDPWoker:
                 config.tp_size
             )
         )
+        self.device_mesh = dist.device_mesh.init_device_mesh(
+            "cuda",
+            mesh_dim_names=("dp", "tp"),
+            mesh_shape=(
+                world_size // config.tp_size,
+                config.tp_size
+            )
+        )
     def _init_weight_context(self, use_meta_tensor: bool = True) -> ContextManager:
         # TODO: why offloading is incompatible with initialization on meta device?
         if any([
@@ -89,4 +97,96 @@ class FSDPWoker:
             scheduler_specific_kwargs=scheduler_config
         )
     
+    def _load_model_to_device(self, device: Union[torch.device, str]):
     
+        if not getattr(self.config, "offload_model", False):
+            return
+
+        torch.cuda.empty_cache()
+        _lazy_init(self.model, self.model)
+        for handle in self.model._all_handles:
+            if handle._offload_params:
+                continue
+            flat_param = handle.flat_param
+            handle.flat_param_to(device, non_blocking=True)
+            flat_param._local_shard = flat_param.data
+        torch.cuda.empty_cache()
+
+    def _load_optimizer_to_device(self, device: Union[torch.device, str]):
+
+        if not getattr(self.config, "offload_optimizer", False):
+            return
+
+        for param_group in self.optimizer.param_groups:
+            for param in param_group["params"]:
+                state = self.optimizer.state[param]
+                for key, value in state.items():
+                    if isinstance(value, torch.Tensor):
+                        state[key] = value.to(
+                            device, non_blocking=True
+                        )
+
+    def _scale_loss(self, loss: torch.Tensor) -> torch.Tensor:
+        # https://github.com/ChenmienTan/RL2/issues/11
+        return self.device_mesh["dp"].size() * loss
+    
+    def _optimizer_step(self) -> int:
+
+        grad_norm = clip_grad_norm_(
+            self.model.parameters(),
+            max_norm=self.config.max_grad_norm
+        )
+        self._load_optimizer_to_device(
+            torch.cuda.current_device()
+        )
+        self.optimizer.step()
+        self.optimizer.zero_grad()
+        self._load_optimizer_to_device("cpu")
+        self.scheduler.step()
+        return grad_norm.item()
+
+    def _get_model_state_dict(
+        self, full_state_dict: bool = False
+    ) -> Dict[str, Any]:
+
+        options = StateDictOptions(
+            full_state_dict=full_state_dict,
+            cpu_offload=True
+        )
+        self._load_model_to_device(torch.cuda.current_device())
+        state_dict = get_model_state_dict(self.model, options=options)
+        self._load_model_to_device("cpu")
+        return state_dict
+
+    def _get_ckpt(self) -> Dict[str, Dict[str, Any]]:
+        return {
+            "optimizer": self.optimizer.state_dict(),
+            "scheduler": self.scheduler.state_dict()
+        }
+
+    def load_ckpt(self, checkpoint_id: str):
+
+        ckpt = self._get_ckpt()
+        dcp.load(ckpt, checkpoint_id=checkpoint_id)
+        self.optimizer.load_state_dict(ckpt["optimizer"])
+        self.scheduler.load_state_dict(ckpt["scheduler"])
+
+    def save_ckpt(self, save_dir: str):
+        
+        self.save_model(f"{save_dir}/model")
+        dcp.save(
+            self._get_ckpt(),
+            checkpoint_id=f"{save_dir}/optimizer_scheduler"
+        )
+
+    def save_model(self, save_dir: str):
+
+        state_dict = self._get_model_state_dict(full_state_dict=True)
+        if dist.get_rank() == 0:
+
+            self.tokenizer.save_pretrained(save_dir)
+            self.model.module.save_pretrained(
+                save_dir, state_dict=state_dict
+            )
+
+        dist.barrier()
--- nanoopd/fsdp/model.py
+++
@@ -1,5 +0,0 @@
-from typing import Dict, Optional
-from omegaconf import DictConfig
-from collections import defaultdict
-import torch
-from transformers import AutoModelForCausalLM, AutoConfig
